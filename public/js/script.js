ef68e217c9dbc2e7c88aaacf197a86e1c8d1384b
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-21 20:58:37 +0530

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,44 @@
+const socket = io();
+
+//checking if the browser contains the geolocation functionality or not
+if (navigator.geolocation) {
+  navigator.geolocation.watchPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    },
+    (error) => {
+      console.error(error);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    }
+  );
+}
+
+//initialise the leaflet map
+const map = L.map("map").setView([0, 0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org./{z}/{x}/{y}.png", {
+  attribution: "Sanjeeban Das",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  map.setView([latitude, longitude], 16);
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+});
+
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});

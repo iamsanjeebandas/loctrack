c7591e4c4c3fa0a1e826bdfc4724d18c7901c466
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-21 22:20:51 +0530

    updated the app file

--- app.js
+++ app.js
@@ -22,4 +22,7 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
-server.listen(5000);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
